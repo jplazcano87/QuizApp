5095dc78fdde6bb6a7e64d3909b38848aba95c2b
Juan Pablo Lazcano <juan.lazcano@schibsted.com>
2018-11-04 22:29:19 -0300

    added case for test

--- QuizAppTests/QuestionViewControllerTest.swift
+++ QuizAppTests/QuestionViewControllerTest.swift
@@ -21,6 +21,18 @@ class QuestionViewControllerTest: XCTestCase {
     func test_optionSelected_withTwoOptionsnotifiesDelegateWithLastSelection() {
         var receivedAnswer = ""
         let sut = makeSut(options: ["A1", "A2"]) { receivedAnswer = $0 }
+       
+        sut.tableView.select(row: 0)
+        XCTAssertEqual(receivedAnswer, "A1")
+        
+        sut.tableView.select(row: 1)
+        XCTAssertEqual(receivedAnswer, "A2")
+    }
+    
+    func test_optionSelected_withMultipleSelectionEnabled_notifiesDelegateSelection() {
+        var receivedAnswer = ""
+        let sut = makeSut(options: ["A1", "A2"]) { receivedAnswer = $0 }
+        
         sut.tableView.select(row: 0)
         XCTAssertEqual(receivedAnswer, "A1")
         
