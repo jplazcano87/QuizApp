892ed242c5e29dc83cfa6d51358a040ea2c7c580
Juan Pablo Lazcano <juan.lazcano@schibsted.com>
2018-11-06 22:14:02 -0300

    Added Test to Results view controller

--- QuizApp/ResultsViewController.swift
+++ QuizApp/ResultsViewController.swift
@@ -1,16 +1,39 @@
 import UIKit
 
-class ResultsViewController: UIViewController {
-    private var summary = ""
-    @IBOutlet var headerLabel: UILabel!
+struct PresentableAnswer {
+    let isCorrect: Bool
+}
+class CorrectAnswerCell: UITableViewCell {
+    
+}
 
-    convenience init(summary: String) {
+class WrongAnswerCell: UITableViewCell {
+    
+}
+
+class ResultsViewController: UIViewController, UITableViewDataSource {
+    @IBOutlet var headerLabel: UILabel!
+    @IBOutlet var tableView: UITableView!
+    private var summary = ""
+    private var answers = [PresentableAnswer]()
+
+    convenience init(summary: String, answers: [PresentableAnswer]) {
         self.init()
         self.summary = summary
+        self.answers = answers
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         headerLabel.text = summary
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return answers.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let answer = answers[indexPath.row]
+        return answer.isCorrect ? CorrectAnswerCell() : WrongAnswerCell()
     }
 }
--- QuizAppTests/ResultsViewControllerTest.swift
+++ QuizAppTests/ResultsViewControllerTest.swift
@@ -1,12 +1,37 @@
-import XCTest
 @testable import QuizApp
+import XCTest
 
 class ResultsViewControllerTest: XCTestCase {
-    
     func test_viewDidLoad_renderSummary() {
-        let sut = ResultsViewController(summary: "a summary")
-        _ = sut.view
-        XCTAssertEqual(sut.headerLabel.text, "a summary")
+        XCTAssertEqual(makeSUT(summary: "a summary").headerLabel.text, "a summary")
+    }
+
+    func test__viewDidLoad_rendersAnswers() {
+        XCTAssertEqual(makeSUT(answers: []).tableView.numberOfRows(inSection: 0), 0)
+        XCTAssertEqual(makeSUT(answers: [makeDummyAnswer()]).tableView.numberOfRows(inSection: 0), 1)
+    }
+
+    func test_viewDidLoad_withCorrectAnswer_renderCorrectAnswerCell(){
+        let sut = makeSUT(answers: [PresentableAnswer(isCorrect: true)])
+        let cell = sut.tableView.cell(at: 0) as? CorrectAnswerCell
+        XCTAssertNotNil(cell)
     }
     
+    func test_viewDidLoad_withWrongAnswer_renderhWrongAnswerCell(){
+        let sut = makeSUT(answers: [PresentableAnswer(isCorrect: false)])
+        let cell = sut.tableView.cell(at: 0) as? WrongAnswerCell
+        XCTAssertNotNil(cell)
+    }
+    
+    // MARK: Helpers
+
+    func makeSUT(summary: String = "", answers: [PresentableAnswer] = []) -> ResultsViewController {
+        let sut = ResultsViewController(summary: summary, answers: answers)
+        _ = sut.view
+        return sut
+    }
+    
+    func makeDummyAnswer() -> PresentableAnswer {
+        return PresentableAnswer(isCorrect: false)
+    }
 }
